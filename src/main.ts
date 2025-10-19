37aa10317865de8dfc0188189f55d5f602f04a69
AKASHNEGI-github <aakashnegi40@gmail.com>
2025-10-19 14:24:30 +0530

    Validate Customer-DTO

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Apply DTO Global Validation
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true, // remove properties that are not defiened in DTO
+    forbidNonWhitelisted: true, // Give Error if find any unknown field
+  }));
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
